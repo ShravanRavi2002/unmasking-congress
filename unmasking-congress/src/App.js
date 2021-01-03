18df0aa6b2af454d403034b7e45340b942fffdf3
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-03 12:58:26 -0500

    Fix front-end JSON parsing

--- unmasking-congress/src/App.js
+++ unmasking-congress/src/App.js
@@ -1,22 +1,36 @@
 import React, {useState} from 'react'
+import {Component} from 'react'
 import './App.css';
 import axios from 'axios';
-function App() {
+class App extends Component {
   state = {
-    data: {},
+    data: null,
   }
-  componentDidMount(){
+  componentDidMount() {
       axios.get('http://localhost:8000')
       .then(response => {
+        console.log(response.data)
         this.setState({
-            details: response.data
+            data: response.data,
           });
       })
-      .catch(err =>{console.error(err)})
+      .catch(err =>{console.error(err)});
   }
-  return(
-    <h1>Hello World</h1>
-  );
+  render(){
+    var outArr = [];
+    if (this.state.data == null)
+      return <div>Loading...</div>
+
+    for (var key in this.state.data) {
+      outArr.push(key + ": " + this.state.data[key])
+    }
+
+    return(
+      outArr.map(key => (
+          <div>{key}</div>
+       ))
+    )
+  }
 }
 
 export default App;
