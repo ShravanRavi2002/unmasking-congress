35f962932562681b1171578f196fc912aeff5f97
himanshubir <68207231+himanshubir@users.noreply.github.com>
2021-01-02 19:49:32 -0500

    Created front-end

--- unmasking-congress/src/App.js
+++ unmasking-congress/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react'
 import './App.css';
-
+import axios from 'axios';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  state = {
+    data: {},
+  }
+  componentDidMount(){
+      axios.get('http://localhost:5000')
+      .then(response => {
+        this.setState({
+            details: response.data
+          });
+      })
+      .catch(err =>{console.error(err)})
+  }
+  return(
+    <h1>Hello World</h1>
   );
 }
 
