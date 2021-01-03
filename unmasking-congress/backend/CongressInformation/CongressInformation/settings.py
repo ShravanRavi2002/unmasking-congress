93316f2143d725e396dc9d2c62c54ab2db94b04e
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-03 12:59:26 -0500

    Use CORS in backend

--- unmasking-congress/backend/CongressInformation/CongressInformation/settings.py
+++ unmasking-congress/backend/CongressInformation/CongressInformation/settings.py
@@ -50,15 +50,18 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',  
+    'django.middleware.common.CommonMiddleware',  
 ]
 
-ROOT_URLCONF = 'CongressInformation.urls'
-
-CORS_ORIGIN_WHITELIST = [
+# CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
     'http://localhost:8080',
 ]
+ROOT_URLCONF = 'CongressInformation.urls'
+
 
 TEMPLATES = [
     {
