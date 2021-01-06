1fe4b7caebbe8f040d6ce70dfc319599c67fffef
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-06 15:00:24 -0500

    Update data formatting for CongressGraph

--- unmasking-congress/src/API.js
+++ unmasking-congress/src/API.js
@@ -4,6 +4,28 @@ export async function sendMonthToBackend(month, setMonthData){
           params:{
               month: String(month)
           }
-      })
-    setMonthData(response.data);
+      });
+    
+      var data = JSON.parse(response.data)
+      var democratData = []
+      var republicanData = []
+
+      for (var repName in data) {
+        let representative = data[repName]
+        
+        if(representative['sentiment'] !== -2 && representative['subjectivity'] !== -2) {
+            if (representative['party'] === 'Democrat') {
+                democratData.push({'x': representative['sentiment'], 'y': representative['subjectivity'], 'label': repName, 'color': 'blue'})
+            } else {
+                republicanData.push({'x': representative['sentiment'], 'y': representative['subjectivity'], 'label': repName, 'color': 'red'})
+            }
+        }
+      }
+      
+      // todo: parse color from group rather than data point (color value below is not the one being read)
+      var democratDictionary = {'id': 'Democrat', 'color': 'blue', 'data': democratData }
+      var republicanDictionary = {'id': 'Republican', 'color': 'red', 'data': republicanData }
+
+    setMonthData([democratDictionary, republicanDictionary]);
+    console.log('in api.js: sendMonthToBackend')
 }
