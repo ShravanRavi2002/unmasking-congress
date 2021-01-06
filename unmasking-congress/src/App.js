570950abda0dcd0df9afd07d1c229891b59aa38d
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-06 15:48:33 -0500

    Revert "Eslint and CountUp"
    This reverts commit 30036ba2a28d9960992e1d9da5cabbf5741c7791.

--- unmasking-congress/src/App.js
+++ unmasking-congress/src/App.js
@@ -2,8 +2,6 @@ import React, { useState } from 'react'
 import './App.css';
 import Timeline from './Timeline.js'
 import CongressGraph from './CongressGraph.js'
-
-import CountUp from 'react-countup'
 // import axios from 'axios';
 
 
@@ -17,12 +15,6 @@ const App = () => {
           <div className = 'timelineContainer'>
             <Timeline setMonthData={setMonthData}/>
           </div>
-          <div>
-            <div style={{display: 'inline', margin: 50, backgroundColor: 'black', color: 'white'}}>
-                <CountUp className="Hospitalized" start={0} end={100}> </ CountUp>
-            </div>
-          </div>
-          
 
           <div className='graphContainer' style={{
             height:'600px',
