2e371fd0ba4bd462777577961b82d19f4d23307b
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-06 14:59:19 -0500

    Add CongressGraph to main App

--- unmasking-congress/src/App.js
+++ unmasking-congress/src/App.js
@@ -1,29 +1,29 @@
 import React, { useState } from 'react'
 import './App.css';
 import Timeline from './Timeline.js'
-import axios from 'axios';
+import CongressGraph from './CongressGraph.js'
+// import axios from 'axios';
+
+
+
 const App = () => {
-    const [monthData, setMonthData] = useState({});
-  
-    // var outArr = [];
-    // if (this.state.data == null)
-    //   return <div>Loading...</div>
-
-    // for (var key in this.state.data) {
-    //   outArr.push(key + ": " + this.state.data[key].sentiment)
-    // }
-
+    const [monthData, setMonthData] = useState([]);
     return(
-      // outArr.map(key => (
-      //     <div>{key}</div>
-      //  ))
-      <div style={{
-        margin:'50px',
-        height: '200px'
-      }}>
-        <Timeline setMonthData={setMonthData}/>
-        {console.log(monthData)}
-      </div>
+
+        <div className='bigContainer'>
+
+          <div className = 'timelineContainer'>
+            <Timeline setMonthData={setMonthData}/>
+          </div>
+
+          <div className='graphContainer' style={{
+            height:'600px',
+          }}>
+          {monthData ? <CongressGraph monthData={monthData}/> : ''}
+          </div>
+
+        </div> 
+
     )
 }
 
