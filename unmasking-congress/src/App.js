2bd518205d28e2b1261efc537af95c1e3169dc5d
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-06 15:56:26 -0500

    Add react-countup

--- unmasking-congress/src/App.js
+++ unmasking-congress/src/App.js
@@ -1,30 +1,42 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 import './App.css';
 import Timeline from './Timeline.js'
 import CongressGraph from './CongressGraph.js'
+
+import Countup from 'react-countup'
 // import axios from 'axios';
 
 
 
 const App = () => {
-    const [monthData, setMonthData] = useState([]);
-    return(
+  const [monthData, setMonthData] = useState([]);
+  return (
 
-        <div className='bigContainer'>
+    <div className='bigContainer'>
 
-          <div className = 'timelineContainer'>
-            <Timeline setMonthData={setMonthData}/>
-          </div>
+      <div className='timelineContainer'>
+        <Timeline setMonthData={setMonthData} />
+      </div>
 
-          <div className='graphContainer' style={{
-            height:'600px',
-          }}>
-          {monthData ? <CongressGraph monthData={monthData}/> : ''}
-          </div>
+      <div display='inline'>
+        <div style={{color: 'white', backgroundColor: 'black'}}>
+          <Countup
+            start={0}
+            end={100}
+          />
+        </div>
 
-        </div> 
+      </div>
 
-    )
+      <div className='graphContainer' style={{
+        height: '600px',
+      }}>
+        {monthData ? <CongressGraph monthData={monthData} /> : ''}
+      </div>
+
+    </div>
+
+  )
 }
 
 export default App;
