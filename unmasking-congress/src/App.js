b01f5d745caa86f90646a571c0e6b8deadc8c6e8
himanshubir <68207231+himanshubir@users.noreply.github.com>
2021-01-09 18:34:48 -0500

    Dark theme and timeline as navbar

--- unmasking-congress/src/App.js
+++ unmasking-congress/src/App.js
@@ -10,69 +10,88 @@ const App = () => {
   const [monthData, setMonthData] = useState([]);
   const [hospitalized=0, setHospitalized] = useState([]);
   const [deaths=0, setDeaths] = useState([]);
-  const [cases=0, setCases] = useState([])
+  const [cases=0, setCases] = useState([]);
   return (
-
+    
     <div className='bigContainer' style={{
-      
-      justifyContent:'center'
-    }}>
-      <div className='headerContainer' style={{textAlign:"center"}}>
-        <h2 >Unmasking Congress</h2>
-        <p>Check out how congressmembers' opinions on masks changed over the course of 2020 as the COVID-19 Pandemic raged on</p>
+      backgroundColor:'#333333',
+      justifyContent:'center',
+      height:'100vh',
+      margin:0
+          }}>
+      <div className='timelineNavBar' style={{
+          width:'100%',
+          height:'50px',
+          position:'fixed',
+          zIndex:'100'
+        }}>
+          <OurStepper setMonthData={setMonthData} setHospitalized={setHospitalized} setDeaths={setDeaths} setCases={setCases}/>
       </div>
-      <div className='timelineContainer' style={{
-        height:'200px',
-        width:'100%'
-      }}>
-        <OurStepper setMonthData={setMonthData} setHospitalized={setHospitalized} setDeaths={setDeaths} setCases={setCases}/>
-      </div>
 
-      <div className='labelContainer' style={{
-        display: 'flex',
-        justifyContent: 'center'
-      }}>
+        <div className='contentContainer' style={{
+          backgroundColor:'#333333',
+          paddingTop: '50px',
+        }}>
 
-        <div style={{textAlign: 'center', justifyContent: 'space-between', margin: 150, fontFamily:'Copperplate'}}>
-          <h2>{'Hospitalized'}</h2>
-          <Countup
-            start={0}
-            end={hospitalized}
-            duration={2}
-            preserveValue={true}
-            separator={','}
-          />
-        </div>
+          <div className='headerContainer' style={{textAlign:"center", color:'#eeeeee'}}>
+            <h2 style={{margin:0, padding:'.5%'}}>Unmasking Congress</h2>
+            <p>Check out how congressmembers' opinions on masks changed over the course of 2020 as the COVID-19 Pandemic raged on</p>
+          </div>
 
-        <div style={{textAlign: 'center', justifyContent: 'center', margin: 150, fontFamily:'Copperplate'}} >
-          <h2>{'Deaths'}</h2>
-          <Countup 
-              start={0}
-              end={deaths}
-              duration={2.5}
-              preserveValue={true}
-              separator={','}
-            />
-        </div>
+          <div className='labelContainer' style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginLeft:'25%',
+            marginRight:'25%'
 
-        <div style={{textAlign: 'center', justifyContent: 'center', margin: 150, fontFamily:'Copperplate'}}>
-          <h2>{'Cases'}</h2>
-          <Countup 
-              start={0}
-              end={cases}
-              duration={3}
-              preserveValue={true}
-              separator={','}
-            />
-        </div>
+            
+          }}>
+            <div style={{color:'yellow'}}>
+              <h2>{'Cases'}</h2>
+              <Countup 
+                  start={0}
+                  end={cases}
+                  duration={2}
+                  preserveValue={true}
+                  separator={','}
+                />
+            </div>
+            <div style={{color:'orange'}}>
+                
+              <h2>{'Hospitalized'}</h2>
+              <Countup
+                start={0}
+                end={hospitalized}
+                duration={2.5}
+                preserveValue={true}
+                separator={','}
+              />
+            </div>
+
+            <div style={{color:'red'}}>
+              <h2>{'Deaths'}</h2>
+              <Countup 
+                  start={0}
+                  end={deaths}
+                  duration={3}
+                  preserveValue={true}
+                  separator={','}
+                />
+            </div>
+
           
-      </div>
+              
+          </div>
 
-      <div className='graphContainer' style={{
-        height: '600px',
-      }}>
-        {monthData ? <CongressGraph monthData={monthData} /> : ''}
-      </div>
+          <div className='graphContainer' style={{
+            height: '70vh',
+            width:'90%',
+            marginLeft:'5%'
+          }}>
+            {monthData ? <CongressGraph monthData={monthData} /> : ''}
+          </div>
+
+        </div>
 
     </div>
 
