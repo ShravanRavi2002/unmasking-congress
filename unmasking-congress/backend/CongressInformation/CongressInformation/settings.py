9173dbfd08a686f7478a19c2ea62175d39b064e2
Akshin Vemana <43317227+AkshinVemana@users.noreply.github.com>
2021-01-10 12:05:43 -0500

    Temporarily disable CORS

--- unmasking-congress/backend/CongressInformation/CongressInformation/settings.py
+++ unmasking-congress/backend/CongressInformation/CongressInformation/settings.py
@@ -55,12 +55,12 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',  
 ]
 
-# CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:3000',
-    'http://localhost:8000',
-    'http://localhost:8080',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+# CORS_ALLOWED_ORIGINS = [
+#     'http://localhost:3000',
+#     'http://localhost:8000',
+#     'http://localhost:8080',
+# ]
 ROOT_URLCONF = 'CongressInformation.urls'
 
 
